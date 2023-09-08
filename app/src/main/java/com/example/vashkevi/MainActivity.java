676a4f449d7498254dce85992a80b007ab5b5fba
Kostya <gnil47311@gmail.com>
2023-09-08 16:09:39 +0300

    Second commit

--- app/src/main/java/com/example/vashkevi/MainActivity.java
+++ app/src/main/java/com/example/vashkevi/MainActivity.java
@@ -9,22 +9,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
 import com.example.vashkevi.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     private LinearLayout board;
+    private Timer time;
     private ArrayList<Button>squares = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
         board = binding.board;
         generateBoard(3,3,board);
         View.OnClickListener clickListener = (view) -> {
@@ -33,13 +42,60 @@ public class MainActivity extends AppCompatActivity {
 
             if(GameInfo.whoIsNext) {
                 button.setText(GameInfo.firstSymbol);
+                int [] comb = findWinner(GameInfo.firstSymbol);
+                if(comb!= null)
+                {
+                    Toast.makeText(this, "Победил "+GameInfo.firstSymbol+ "!",Toast.LENGTH_LONG).show();
+                    win(comb);
+
+
+
+                }
             }
             else {
                 button.setText(GameInfo.secondSymbol);
+                int [] comb = findWinner(GameInfo.secondSymbol);
+                if(comb!=null)
+                {
+                    Toast.makeText(this, "Победил "+GameInfo.secondSymbol+ "!",Toast.LENGTH_LONG).show();
+                    win(comb);
+
+
+
+                }
             }
             GameInfo.whoIsNext = !GameInfo.whoIsNext;
         };
         setListenerToSquares(clickListener);
+        //initClearBordBtn();
+    }
+    public void initClearBordBtn()
+    {
+        binding.clearBoardValue.setOnClickListener(view -> {
+            GameInfo.whoIsNext= true;
+            Toast.makeText(this,"New Game",Toast.LENGTH_LONG).show();
+            for (Button square:squares) {
+                square.setText("");
+                square.setBackgroundTintList(
+                        ContextCompat.getColorStateList(
+                                getApplicationContext(),
+                                R.color.gray));
+
+            }
+        });
+
+    }
+    public void win(int[]winCombo)
+    {
+        for(int i =0; i<winCombo.length;i++)
+        {
+            squares.get(winCombo[i]).setBackgroundTintList(
+                    ContextCompat.getColorStateList(
+                            getApplicationContext(),
+                            R.color.teal_200));;
+        }
+
+
     }
     private void setListenerToSquares(View.OnClickListener listener){
         for (int i =0; i< squares.size();i++)
@@ -73,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                             R.color.gray));
 
                     button.setWidth(convertToDp(50));
-                    button.setHeight(convertToDp(50));
+                    button.setHeight(convertToDp(90));
                     rowContainer.addView(button);
                     squares.add(button);
         }
@@ -85,5 +141,30 @@ public class MainActivity extends AppCompatActivity {
         float density = getApplicationContext()
                 .getResources().getDisplayMetrics().density;
         return (int)(digit*density+0/5);
+    }
+    private int[] findWinner(String symbol)
+    {
+
+        for (int i =0; i<GameInfo.winCombination.length;i++)
+        {
+            boolean findComb = true;
+            for(int j =0;j<GameInfo.winCombination[1].length;j++)
+            {
+               int idBtn = GameInfo.winCombination[i][j];
+               if(!squares.get(idBtn).getText().equals(symbol))
+               {
+                   findComb = false;
+                   break;
+
+
+               }
+            }
+            if(findComb) return GameInfo.winCombination[i];
+
+        }
+        return null;
+
+
+
     }
 }
